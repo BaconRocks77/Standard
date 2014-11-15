a1626cdfe19a8650c68a64c817adde11a2ea9d0d
hii488 <hii488@btinternet.com>
2014-11-15 08:06:19 +0000

    Huge update
    Added lots

--- src/main/java/me/hii488/moRandomStuff/Registries/ItemRegistry.java
+++ src/main/java/me/hii488/moRandomStuff/Registries/ItemRegistry.java
@@ -22,8 +22,8 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 public class ItemRegistry{
 
-	public static Item silverIngot;
-    
+    public static Item silverIngot;
+    public static Item gonumiteIngot;
     
     //Silver items
     public static ItemSword silverSword;
@@ -31,6 +31,18 @@ public class ItemRegistry{
     public static ItemPickaxe silverPick;
     public static ItemHoe silverHoe;
     public static ItemSpade silverSpade;
+    
+    //Gonumite items
+    public static ItemSword gonumiteSword;
+    public static ItemAxe gonumiteAxe;
+    public static ItemPickaxe gonumitePick;
+    public static ItemHoe gonumiteHoe;
+    public static ItemSpade gonumiteSpade;
+    
+  //Gears
+    public static Item woodGear;
+    public static Item stoneGear;
+    public static Item ironGear;
     
     //Misc weapons
     public static ItemSword bootySlapper;
@@ -43,19 +55,50 @@ public class ItemRegistry{
     public static ItemFood chips;
     public static ItemFood enderFlesh;
     public static ItemFood cookedFlesh;
+    public static ItemFood sausage;
+    public static ItemFood breadSlice;
+    public static ItemFood hotDog;
+    public static ItemFood friedEgg;
+    public static ItemFood strawberry;
+    public static ItemFood plainDoughnut;
+    public static ItemFood chocDoughnut;
+    public static ItemFood sugarDoughnut;
+    public static ItemFood strawberryDoughnut;
+    public static ItemFood butterEyeball;
+    public static ItemFood ironApple;
+    public static ItemFood honey;
+    public static ItemFood gummySkull;
+    public static ItemFood chewyBones;
+    public static ItemFood lollipop;
+    public static ItemFood enderpop;
+    public static ItemFood popcorn;
     
     
     //Misc
     public static Item WitheredBones;
     public static Item WitheredBoneMeal;
     public static Item uncookedChips;
+    public static Item metalBoat;
+    public static Item dough;
+    public static Item doughnutCutter;
+    public static Item eyeBall;
+    public static Item butter;
+    public static Item skull;
+    public static Item boringCandy;
+    public static Item corn;
     
-    //Just Weird things
+    //Misc other stuff
     public static ItemSword kitchenKnife;
     
     public static void ItemReg(){
     	//Ingots
     	silverIngot = new SimpleItem().setUnlocalizedName("silverIngot");
+    	gonumiteIngot = new SimpleItem().setUnlocalizedName("gonumiteIngot");
+    	
+    	//Gears
+    	woodGear = new SimpleItem().setUnlocalizedName("woodGear");
+    	ironGear = new SimpleItem().setUnlocalizedName("ironGear");
+    	stoneGear = new SimpleItem().setUnlocalizedName("stoneGear");
     	
     	//SilverItems
     	silverSword = (ItemSword) new Sword(TaAMaterialRegistry.silver).setUnlocalizedName("silverSword");
@@ -63,6 +106,13 @@ public class ItemRegistry{
     	silverHoe = (ItemHoe) new Hoe(TaAMaterialRegistry.silver).setUnlocalizedName("silverHoe");
     	silverPick = (ItemPickaxe) new PickAxe(TaAMaterialRegistry.silver).setUnlocalizedName("silverPick");
     	silverSpade = (ItemSpade) new Spade(TaAMaterialRegistry.silver).setUnlocalizedName("silverSpade");
+    	
+    	//gonumite Items
+    	gonumiteSword = (ItemSword) new Sword(TaAMaterialRegistry.gonumite).setUnlocalizedName("gonumiteSword");
+    	gonumiteAxe = (ItemAxe) new Axe(TaAMaterialRegistry.gonumite).setUnlocalizedName("gonumiteAxe");
+    	gonumiteHoe = (ItemHoe) new Hoe(TaAMaterialRegistry.gonumite).setUnlocalizedName("gonumiteHoe");
+    	gonumitePick = (ItemPickaxe) new PickAxe(TaAMaterialRegistry.gonumite).setUnlocalizedName("gonumitePick");
+    	gonumiteSpade = (ItemSpade) new Spade(TaAMaterialRegistry.gonumite).setUnlocalizedName("gonumiteSpade");
     	
     	//Food
     	squidMeat = (ItemFood) new JustFood(1,0.5f,false).setUnlocalizedName("squidMeat");
@@ -72,11 +122,35 @@ public class ItemRegistry{
     	chips = (ItemFood) new JustFood(2,1.0f,false).setUnlocalizedName("chips");
     	enderFlesh = (ItemFood) new EnderFlesh(2,1.0f,false).setUnlocalizedName("EnderFlesh");
     	cookedFlesh = (ItemFood) new EnderFlesh(2,1.0f,false).setUnlocalizedName("CookedFlesh");
+    	sausage = (ItemFood) new JustFood(1,1.0f,true).setUnlocalizedName("sausage");
+    	breadSlice = (ItemFood) new JustFood(1,0.2f,false).setUnlocalizedName("breadSlice");
+    	hotDog = (ItemFood) new JustFood(4,3.5f,false).setUnlocalizedName("hotDog");
+    	friedEgg = (ItemFood) new JustFood(2, 0.5f, false).setUnlocalizedName("friedEgg");
+    	strawberry = (ItemFood) new JustFood(1,0f,false).setUnlocalizedName("strawberry");
+    	plainDoughnut = (ItemFood) new JustFood(2,0.5f,false).setUnlocalizedName("plainDoughnut");
+    	chocDoughnut = (ItemFood) new JustFood(2,1f,false).setUnlocalizedName("chocDoughnut");
+    	strawberryDoughnut = (ItemFood) new JustFood(3,0.5f,false).setUnlocalizedName("strawberryDoughnut");
+    	sugarDoughnut = (ItemFood) new JustFood(4,-0.5f,false).setUnlocalizedName("sugarDoughnut");
+    	butterEyeball = (ItemFood) new JustFood(2, 0.5f,false).setUnlocalizedName("butterEyeball");
+    	ironApple = (ItemFood) new IronApple(2,1f,false).setAlwaysEdible().setUnlocalizedName("ironApple");
+    	honey = (ItemFood) new JustFood(2, 0.5f,false).setUnlocalizedName("honey");
+    	gummySkull = (ItemFood) new JustFood(4, -0.5f,false).setUnlocalizedName("gummySkull");
+    	chewyBones = (ItemFood) new JustFood(4, -0.5f,false).setUnlocalizedName("chewyBones");
+    	lollipop = (ItemFood) new JustFood(5, -1f,false).setUnlocalizedName("lollipop");
+    	enderpop = (ItemFood) new EnderFlesh(5, -1f,false).setUnlocalizedName("enderpop");
+    	popcorn = (ItemFood) new EnderFlesh(3, 0f,false).setUnlocalizedName("popcorn");
     	
     	//Misc
     	WitheredBones = new SimpleItem().setUnlocalizedName("WitheredBones");
     	WitheredBoneMeal = new WitheredBoneMeal().setUnlocalizedName("WitheredBoneMeal");
     	uncookedChips = new SimpleItem().setUnlocalizedName("uncookedChips").setCreativeTab(TabRegistry.MRFtab);
+    	metalBoat = new MetalBoat().setUnlocalizedName("metalBoat");
+    	doughnutCutter = new SimpleItem().setUnlocalizedName("doughnutCutter");
+    	eyeBall = new SimpleItem().setUnlocalizedName("eyeBall");
+    	butter = new SimpleItem().setUnlocalizedName("butter");
+    	skull = new SimpleItem().setUnlocalizedName("skull");
+    	boringCandy = new SimpleItem().setUnlocalizedName("unsetCandy");
+    	corn = new SimpleItem().setUnlocalizedName("corn");
     	
     	//Misc Weapons
     	bootySlapper = (ItemSword) new BootySlapper(TaAMaterialRegistry.bootySlapper).setUnlocalizedName("BootySlapper");
@@ -85,12 +159,23 @@ public class ItemRegistry{
     	kitchenKnife = (ItemSword) new KitchenKnife(TaAMaterialRegistry.KitchenKnife).setUnlocalizedName("KitchenKnife");
 
     	GameRegistry.registerItem(silverIngot, "silverIngot");
+    	GameRegistry.registerItem(gonumiteIngot, "gonumiteIngot");
+    	
+    	GameRegistry.registerItem(woodGear, "woodGear");
+    	GameRegistry.registerItem(ironGear, "ironGear");
+    	GameRegistry.registerItem(stoneGear, "stoneGear");
     	
     	GameRegistry.registerItem(silverAxe, "silverAxe");
     	GameRegistry.registerItem(silverSword, "silverSword");
     	GameRegistry.registerItem(silverHoe, "silverHoe");
     	GameRegistry.registerItem(silverPick, "silverPick");
     	GameRegistry.registerItem(silverSpade, "silverSpade");
+    	
+    	GameRegistry.registerItem(gonumiteAxe, "gonumiteAxe");
+    	GameRegistry.registerItem(gonumiteSword, "gonumiteSword");
+    	GameRegistry.registerItem(gonumiteHoe, "gonumiteHoe");
+    	GameRegistry.registerItem(gonumitePick, "gonumitePick");
+    	GameRegistry.registerItem(gonumiteSpade, "gonumiteSpade");
     	
     	GameRegistry.registerItem(squidMeat, "squidMeat");
     	GameRegistry.registerItem(cookedSquidMeat, "cookedSquidMeat");
@@ -99,10 +184,31 @@ public class ItemRegistry{
     	GameRegistry.registerItem(chips, "Chips");
     	GameRegistry.registerItem(enderFlesh, "EnderFlesh");
     	GameRegistry.registerItem(cookedFlesh, "CookedFlesh");
+    	GameRegistry.registerItem(sausage, "sausage");
+    	GameRegistry.registerItem(breadSlice, "breadSlice");
+    	GameRegistry.registerItem(hotDog, "hotDog");
+    	GameRegistry.registerItem(friedEgg, "friedEgg");
+    	GameRegistry.registerItem(strawberry, "strawberry");
+    	GameRegistry.registerItem(plainDoughnut, "plainDoughnut");
+    	GameRegistry.registerItem(chocDoughnut, "chocDoughtnut");
+    	GameRegistry.registerItem(strawberryDoughnut, "strawberryDoughnut");
+    	GameRegistry.registerItem(sugarDoughnut, "sugarDoughnut");
+    	GameRegistry.registerItem(butterEyeball, "butterEyeball");
+    	GameRegistry.registerItem(ironApple, "ironApple");
+    	GameRegistry.registerItem(popcorn, "popcorn");
+    	GameRegistry.registerItem(lollipop, "lollipop");
+    	GameRegistry.registerItem(enderpop, "enderpop");
     	
     	GameRegistry.registerItem(WitheredBones, "WitheredBones");
     	GameRegistry.registerItem(WitheredBoneMeal, "WitheredBoneMeal");
     	GameRegistry.registerItem(uncookedChips, "UncookedChips");
+    	GameRegistry.registerItem(metalBoat, "metalBoat");
+    	GameRegistry.registerItem(doughnutCutter, "doughnutCutter");
+    	GameRegistry.registerItem(eyeBall, "eyeBall");
+    	GameRegistry.registerItem(butter, "butter");
+    	GameRegistry.registerItem(skull, "skull");
+    	GameRegistry.registerItem(boringCandy, "unsetCandy");
+    	GameRegistry.registerItem(corn, "corn");
     	
     	GameRegistry.registerItem(bootySlapper, "BootySlapper");
     	GameRegistry.registerItem(kitchenKnife, "KitchenKnife");
