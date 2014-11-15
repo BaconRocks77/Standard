ebc1c017e0628b0b00fc1100c0bc7f4a125d7e1e
Laurence <hii488@btinternet.com>
2014-11-15 10:45:13 +0000

    Changed ModelBiped() to ModelZombie()
    Warning - will break textures

--- src/main/java/me/hii488/moRandomStuff/main/ClientProxy.java
+++ src/main/java/me/hii488/moRandomStuff/main/ClientProxy.java
@@ -11,9 +11,9 @@ import cpw.mods.fml.client.registry.RenderingRegistry;
 
 public class ClientProxy extends ServerProxy{
 	public void registerRenderThings(){
-		RenderingRegistry.registerEntityRenderingHandler(EntityNetherZombie.class, new RenderEntityNetherZombie(new ModelBiped(), 0));
-		RenderingRegistry.registerEntityRenderingHandler(EntityTntZombie.class, new RenderEntityTntZombie(new ModelBiped(), 0));
-		RenderingRegistry.registerEntityRenderingHandler(EntityEnderZombie.class, new RenderEntityEnderZombie(new ModelBiped(), 0));
+		RenderingRegistry.registerEntityRenderingHandler(EntityNetherZombie.class, new RenderEntityNetherZombie(new ModelZombie(), 0));
+		RenderingRegistry.registerEntityRenderingHandler(EntityTntZombie.class, new RenderEntityTntZombie(new ModelZombie(), 0));
+		RenderingRegistry.registerEntityRenderingHandler(EntityEnderZombie.class, new RenderEntityEnderZombie(new ModelZombie(), 0));
 		
 		RenderingRegistry.registerEntityRenderingHandler(EntityMetalBoat.class, new RenderEntityMetalBoat());
 	}
