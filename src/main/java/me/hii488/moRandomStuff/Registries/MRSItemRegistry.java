a196e22acd32d36c093b096d5b63787bb7d46225
Laurence <hii488@btinternet.com>
2014-11-16 14:40:02 +0000

    Updated with more items

--- src/main/java/me/hii488/moRandomStuff/Registries/MRSItemRegistry.java
+++ src/main/java/me/hii488/moRandomStuff/Registries/MRSItemRegistry.java
@@ -24,6 +24,10 @@ public class MRSItemRegistry{
 
     public static Item silverIngot;
     public static Item gonumiteIngot;
+    public static Item dragoniteIngot;
+	public static Item enuriumIngot;
+	public static Item zayuamiteIngot;
+	public static Item binoiumIngot;
     
     //Silver items
     public static ItemSword silverSword;
@@ -38,6 +42,27 @@ public class MRSItemRegistry{
     public static ItemPickaxe gonumitePick;
     public static ItemHoe gonumiteHoe;
     public static ItemSpade gonumiteSpade;
+    
+      //dragonite items
+    public static ItemSword dragoniteSword;
+    public static ItemAxe dragoniteAxe;
+    public static ItemPickaxe dragonitePick;
+    public static ItemHoe dragoniteHoe;
+    public static ItemSpade dragoniteSpade;
+    
+  //enurium items
+    public static ItemSword enuriumSword;
+    public static ItemAxe enuriumAxe;
+    public static ItemPickaxe enuriumPick;
+    public static ItemHoe enuriumHoe;
+    public static ItemSpade enuriumSpade;
+    
+  //binoium items
+    public static ItemSword binoiumSword;
+    public static ItemAxe binoiumAxe;
+    public static ItemPickaxe binoiumPick;
+    public static ItemHoe binoiumHoe;
+    public static ItemSpade binoiumSpade;
     
   //Gears
     public static Item woodGear;
@@ -94,6 +119,10 @@ public class MRSItemRegistry{
     	//Ingots
     	silverIngot = new SimpleItem().setUnlocalizedName("silverIngot");
     	gonumiteIngot = new SimpleItem().setUnlocalizedName("gonumiteIngot");
+    	dragoniteIngot = new SimpleItem().setUnlocalizedName("dragoniteIngot");
+    	enuriumIngot = new SimpleItem().setUnlocalizedName("enuriumIngot");
+    	zayuamiteIngot = new SimpleItem().setUnlocalizedName("zayuamiteIngot");
+    	binoiumIngot = new SimpleItem().setUnlocalizedName("gonumiteIngot");
     	
     	//Gears
     	woodGear = new SimpleItem().setUnlocalizedName("woodGear");
@@ -113,6 +142,27 @@ public class MRSItemRegistry{
     	gonumiteHoe = (ItemHoe) new Hoe(TaAMaterialRegistry.gonumite).setUnlocalizedName("gonumiteHoe");
     	gonumitePick = (ItemPickaxe) new PickAxe(TaAMaterialRegistry.gonumite).setUnlocalizedName("gonumitePick");
     	gonumiteSpade = (ItemSpade) new Spade(TaAMaterialRegistry.gonumite).setUnlocalizedName("gonumiteSpade");
+    	
+    	//dragonite Items
+    	dragoniteSword = (ItemSword) new Sword(TaAMaterialRegistry.dragonite).setUnlocalizedName("dragoniteSword");
+    	dragoniteAxe = (ItemAxe) new Axe(TaAMaterialRegistry.dragonite).setUnlocalizedName("dragoniteAxe");
+    	dragoniteHoe = (ItemHoe) new Hoe(TaAMaterialRegistry.dragonite).setUnlocalizedName("dragoniteHoe");
+    	dragonitePick = (ItemPickaxe) new PickAxe(TaAMaterialRegistry.dragonite).setUnlocalizedName("dragonitePick");
+    	dragoniteSpade = (ItemSpade) new Spade(TaAMaterialRegistry.dragonite).setUnlocalizedName("dragoniteSpade");
+    	
+    	//enurium Items
+    	enuriumSword = (ItemSword) new Sword(TaAMaterialRegistry.enurium).setUnlocalizedName("enuriumSword");
+    	enuriumAxe = (ItemAxe) new Axe(TaAMaterialRegistry.enurium).setUnlocalizedName("enuriumAxe");
+    	enuriumHoe = (ItemHoe) new Hoe(TaAMaterialRegistry.enurium).setUnlocalizedName("enuriumHoe");
+    	enuriumPick = (ItemPickaxe) new PickAxe(TaAMaterialRegistry.enurium).setUnlocalizedName("enuriumPick");
+    	enuriumSpade = (ItemSpade) new Spade(TaAMaterialRegistry.enurium).setUnlocalizedName("enuriumSpade");
+    	
+    	//binoium Items
+    	binoiumSword = (ItemSword) new Sword(TaAMaterialRegistry.binoium).setUnlocalizedName("binoiumSword");
+    	binoiumAxe = (ItemAxe) new Axe(TaAMaterialRegistry.binoium).setUnlocalizedName("binoiumAxe");
+    	binoiumHoe = (ItemHoe) new Hoe(TaAMaterialRegistry.binoium).setUnlocalizedName("binoiumHoe");
+    	binoiumPick = (ItemPickaxe) new PickAxe(TaAMaterialRegistry.binoium).setUnlocalizedName("binoiumPick");
+    	binoiumSpade = (ItemSpade) new Spade(TaAMaterialRegistry.binoium).setUnlocalizedName("binoiumSpade");
     	
     	//Food
     	squidMeat = (ItemFood) new JustFood(1,0.5f,false).setUnlocalizedName("squidMeat");
@@ -160,6 +210,10 @@ public class MRSItemRegistry{
 
     	GameRegistry.registerItem(silverIngot, "silverIngot");
     	GameRegistry.registerItem(gonumiteIngot, "gonumiteIngot");
+    	GameRegistry.registerItem(dragoniteIngot, "dragoniteIngot");
+    	GameRegistry.registerItem(enuriumIngot, "enuriumIngot");
+    	GameRegistry.registerItem(binoiumIngot, "binoiumIngot");
+    	GameRegistry.registerItem(zayuamiteIngot, "zayuamiteIngot");
     	
     	GameRegistry.registerItem(woodGear, "woodGear");
     	GameRegistry.registerItem(ironGear, "ironGear");
@@ -176,6 +230,24 @@ public class MRSItemRegistry{
     	GameRegistry.registerItem(gonumiteHoe, "gonumiteHoe");
     	GameRegistry.registerItem(gonumitePick, "gonumitePick");
     	GameRegistry.registerItem(gonumiteSpade, "gonumiteSpade");
+    	
+    	GameRegistry.registerItem(dragoniteAxe, "dragoniteAxe");
+    	GameRegistry.registerItem(dragoniteSword, "dragoniteSword");
+    	GameRegistry.registerItem(dragoniteHoe, "dragoniteHoe");
+    	GameRegistry.registerItem(dragonitePick, "dragonitePick");
+    	GameRegistry.registerItem(dragoniteSpade, "dragoniteSpade");
+    	
+    	GameRegistry.registerItem(enuriumAxe, "enuriumAxe");
+    	GameRegistry.registerItem(enuriumSword, "enuriumSword");
+    	GameRegistry.registerItem(enuriumHoe, "enuriumHoe");
+    	GameRegistry.registerItem(enuriumPick, "enuriumPick");
+    	GameRegistry.registerItem(enuriumSpade, "enuriumSpade");
+    	
+    	GameRegistry.registerItem(binoiumAxe, "binoiumAxe");
+    	GameRegistry.registerItem(binoiumSword, "binoiumSword");
+    	GameRegistry.registerItem(binoiumHoe, "binoiumHoe");
+    	GameRegistry.registerItem(binoiumPick, "binoiumPick");
+    	GameRegistry.registerItem(binoiumSpade, "binoiumSpade");
     	
     	GameRegistry.registerItem(squidMeat, "squidMeat");
     	GameRegistry.registerItem(cookedSquidMeat, "cookedSquidMeat");
