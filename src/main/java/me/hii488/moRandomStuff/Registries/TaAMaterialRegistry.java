1b7a047b44148b23cdee88dd4bfc48df60a2e732
Laurence <hii488@btinternet.com>
2014-11-16 14:35:32 +0000

    Updated with more ToolMaterials

--- src/main/java/me/hii488/moRandomStuff/Registries/TaAMaterialRegistry.java
+++ src/main/java/me/hii488/moRandomStuff/Registries/TaAMaterialRegistry.java
@@ -6,7 +6,10 @@ import net.minecraftforge.common.util.EnumHelper;
 public class TaAMaterialRegistry {
 	 //Tool Materials
     public static ToolMaterial silver = EnumHelper.addToolMaterial("Silver", 2, 350, 5.5F, 1.5F, 100);
-     public static ToolMaterial gonumite = EnumHelper.addToolMaterial("Gonumite", 2, 350, 6.5F, 2.5F, 18);
+    public static ToolMaterial gonumite = EnumHelper.addToolMaterial("Gonumite", 2, 350, 6.5F, 2.5F, 18);
+    public static ToolMaterial dragonite = EnumHelper.addToolMaterial("Dragonite", 4, 10000, 10F, 4F, 50);
+    public static ToolMaterial enurium = EnumHelper.addToolMaterial("Enurium", 2, 350, 6.5F, 2.5F, 75);
+    public static ToolMaterial binoium = EnumHelper.addToolMaterial("Binoium", 1, 150, 5.3F, 1.5F, 9);
     
     public static ToolMaterial bootySlapper = EnumHelper.addToolMaterial("BootySlapper", 0, 300, -1.0f, -2.0f, 1);
     public static ToolMaterial KitchenKnife = EnumHelper.addToolMaterial("KitchenKnife", 1, 300, 0f, -1.0f, 10);
