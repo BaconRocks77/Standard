d6b8b6efdf71311570446a4ce9bea5fe8e6818e1
Laurence <hii488@btinternet.com>
2014-12-23 22:40:11 +0000

    Just changing 1 thing

--- src/main/java/me/hii488/moRandomStuff/main/Main.java
+++ src/main/java/me/hii488/moRandomStuff/main/Main.java
@@ -20,7 +20,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class Main {
 
 	public static final String MODID = "mrs";
-    public static final String VERSION = "0.a";
+    public static final String VERSION = "WIP b";
 
     @Instance(Main.MODID)
     public static Main modinstance;
